6700de8508be1acb76fcd110c63dda076585aba7
handa-kun <Whitey.A@yandex.ru>
2022-12-04 18:46:22 +0300

    feat: add module.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
+import classes from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={classes.header}>
             <img src='https://www.pngkit.com/png/detail/178-1784079_the-office-office-tv-show-logo.png' alt='logo' />
         </header>
     )
 }
  
-export default Header
+export default Header;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,22 +1,24 @@
+import classes from './Profile.module.css'
+
 const Profile = () => {
     return (
-    <div className='content'>
-        <div>
+    <div className={classes.content}>
+        <div className={classes.header_content}>
           <img src='https://i0.wp.com/lokagraph.com/wp-content/uploads/2018/05/the-office-dunder-Mifflin-building-warehouse.jpg?fit=1024%2C683' alt='logo'></img>
         </div>
         <div>
           <img src='' />
         </div>
-        <div>
+        <div className={classes.posts}>
           My posts
           <div>
             New post
           </div>
           <div>
-            <div>
+            <div className={classes.item}>
               post
             </div>
-            <div>
+            <div className={classes.item}>
               post
             </div>
           </div>
