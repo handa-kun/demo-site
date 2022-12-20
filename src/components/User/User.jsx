28235b9c7e03286348c1b064070e911be4da57aa
handa-kun <Whitey.A@yandex.ru>
2022-12-20 16:50:54 +0300

    feat: added class.component

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,47 +1,47 @@
+import React from "react";
 import axios from "axios";
 import classes from '../User/User.module.css'
 
-let Users = (props) => {
+class Users extends React.Component {
+    getUsers = () => {
+        if (this.props.users.length === 0) {
 
-    let getUsers = () => {
-        if (props.users.length === 0) {
-    
             axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                props.set_users(response.data.items);
-            });
+                .then(response => {
+                    this.props.set_users(response.data.items);
+                });
         };
 
     }
-
-    
-    return <div>
-        <button onClick={getUsers}>Get</button>
-        {
-            props.users.map( u => <div key={u.id}> 
-                <span>
-                    <div>
-                        <img src={'https://writers.com/wp-content/uploads/2022/06/icon-poetry.png'} className={classes.img} alt='logo'/>
-                    </div>
-                    <div>
-                        { u.followed ? 
-                        <button onClick={ () => props.unfollow(u.id)}>Unfollowed</button> : 
-                        <button onClick={ () => props.follow(u.id)}>Followed</button>}
-                    </div>
-                </span>
-                <span>
+    render() {
+        return <div>
+            <button onClick={this.getUsers}>Get</button>
+            {
+                this.props.users.map(u => <div key={u.id}>
                     <span>
-                        <div>{u.name}</div>
-                        <div>{u.status}</div>
-                    </span>     
-                    <span>
-                        {/* <div>{u.location.country}</div>
-                        <div>{u.location.city}</div> */}
+                        <div>
+                            <img src={'https://writers.com/wp-content/uploads/2022/06/icon-poetry.png'} className={classes.img} alt='logo' />
+                        </div>
+                        <div>
+                            {u.followed ?
+                                <button onClick={() => this.props.unfollow(u.id)}>Unfollowed</button> :
+                                <button onClick={() => this.props.follow(u.id)}>Followed</button>}
+                        </div>
                     </span>
-                </span>
-            </div>)
-        }
-    </div>
+                    <span>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        <span>
+                            {/* <div>{u.location.country}</div>
+                        <div>{u.location.city}</div> */}
+                        </span>
+                    </span>
+                </div>)
+            }
+        </div>
+    }
 }
 
 export default Users;
