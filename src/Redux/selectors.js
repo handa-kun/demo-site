bd2ca541c8f50ba4460a94ee07621764396e387f
handa-kun <Whitey.A@yandex.ru>
2023-02-03 18:30:04 +0300

    feat: add createSelector

--- src/Redux/selectors.js
+++ src/Redux/selectors.js
@@ -1,6 +1,13 @@
-export const takeUsers = (state) => {
+import { createSelector } from 'reselect';
+
+const getUsers = (state) => {
     return state.usersPage.users;
 };
+
+export const takeUsers = createSelector(getUsers,
+    (users) => {
+        return users.filter(u => true);
+    });
 
 export const getPageSize = (state) => {
     return state.usersPage.pageSize;
